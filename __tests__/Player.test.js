7fd70f585ffd32f2265df0d6e2552bfb04260a1e
Hilary Richardson <hilary.richardson@gmail.com>
2021-03-13 15:28:21 -0800

    additional player tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -70,3 +70,33 @@ test("subtracts from player's health", () => {
 
     expect(player.health).toBe(0);
 });
+
+// verifies that a player's attack value is within range
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+// check that potion correctly added
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+// use up potion from inventory
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
